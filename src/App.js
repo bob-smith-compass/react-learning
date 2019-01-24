fdef3681f0871da7c56526458732a5097b0d2a34
David Shams <david.shams@gmail.com>
2019-01-24 13:51:06 -0600

    404 added React is Awsome!

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./components/pages/About";
 import Home from "./components/pages/Home";
 import {Link} from 'react-router-dom';
+import Notfound from "./components/pages/Notfound";
 
 class App extends Component {
   render() {
@@ -33,6 +34,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
+            <Route component={Notfound} />
             <Link to="/">Home</Link> | <Link to="/about:id">About</Link>
 
           </Switch>
